21093aed039df078bca302588db8fc389c3c4f93
Mr0Hadi <hadibalaei.dev7@gmail.com>
2025-12-03 00:17:45 +0330

    modified ReservationService class

--- Step-02-with-OOD/src/services/ReservationService.java
+++ Step-02-with-OOD/src/services/ReservationService.java
@@ -9,35 +9,35 @@ public class ReservationService {
     private PaymentProcessor paymentProcessor = new PaymentProcessor();
 
     public void makeReservation(Reservation res, PaymentMethods paymentType, Notifier notifier){
-        System.out.println("Processing reservation for " + res.customer.name);
+        System.out.println("Processing reservation for " + res.getCustomer().getName());
 
-        if(res.customer.city.equals("Paris")){
+        if(res.getCustomer().getCity().equals("Paris")){
             System.out.println("Apply city discount for Paris!");
-            res.room.price *= 0.9;
+            res.getRoom().setPrice(res.getRoom().getPrice() * 0.9);
         }
 
         switch (paymentType){
             case CARD:
-                paymentProcessor.payByCard(res.totalPrice());
+                paymentProcessor.payByCard(res.getNights() * res.getRoom().getPrice());
                 break;
             case PAYPAL:
-                paymentProcessor.payByPayPal(res.totalPrice());
+                paymentProcessor.payByPayPal(res.getNights() * res.getRoom().getPrice());
                 break;
             case CASH:
-                paymentProcessor.payByCash(res.totalPrice());
+                paymentProcessor.payByCash(res.getNights() * res.getRoom().getPrice());
                 break;
         }
 
         System.out.println("----- INVOICE -----");
-        System.out.println("hotel.Customer: " + res.customer.name);
-        System.out.println("hotel.Room: " + res.room.number + " (" + res.room.type + ")");
-        System.out.println("Total: " + res.totalPrice());
+        System.out.println("hotel.Customer: " + res.getCustomer().getName());
+        System.out.println("hotel.Room: " + res.getRoom().getNumber() + " (" + res.getRoom().getType() + ")");
+        System.out.println("Total: " + res.getNights() * res.getRoom().getPrice());
         System.out.println("-------------------");
 
        switch (this.notifier){
            case EMAIL :
            EmailSender emailSender = new EmailSender();
-           emailSender.sendEmail(res.customer.email, "Your reservation confirmed!");
+           emailSender.sendEmail(res.getCustomer().getEmail(), "Your reservation confirmed!");
            break;
            default:
                System.out.println("There is no Message Provider");
