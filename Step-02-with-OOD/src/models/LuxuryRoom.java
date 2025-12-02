be7749a3f6ccf13651541022775bb43afa29bfc0
Mr0Hadi <hadibalaei.dev7@gmail.com>
2025-12-02 23:49:17 +0330

    Rewriting the module classes of Customer, LuxuryRoom, Reservation and Room

--- Step-02-with-OOD/src/models/LuxuryRoom.java
+++ Step-02-with-OOD/src/models/LuxuryRoom.java
@@ -3,7 +3,4 @@ public class LuxuryRoom extends Room {
     public LuxuryRoom(String number, double price){
         super(number, "luxury", price);
     }
-    public void addFreeDinner(){
-        System.out.println("Free dinner added for luxury room " + number);
-    }
 }
--- Step-02-with-OOD/src/models/Reservation.java
+++ Step-02-with-OOD/src/models/Reservation.java
@@ -1,16 +1,25 @@
 package models;
 
 public class Reservation {
-    public Room room;
-    public Customer customer;
-    public int nights;
+    private Room room;
+    private Customer customer;
+    private int nights;
 
     public Reservation(Room r, Customer c, int nights) {
         this.room = r;
         this.customer = c;
         this.nights = nights;
     }
-    public double totalPrice(){
-        return room.price * nights;
+
+    public int getNights() {
+        return nights;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public Customer getCustomer() {
+        return customer;
     }
 }
